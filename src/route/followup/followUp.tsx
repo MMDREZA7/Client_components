f4b5b7f9e8866df90cb42bfee6cada39cb929f74
MMDREZA <mheidari831003.gameGmail.com>
2025-02-12 15:53:21 +0330

    setting value on inputs when user click on one of rows in table added

--- src/route/followup/followUp.tsx
+++ src/route/followup/followUp.tsx
@@ -5,10 +5,16 @@ import {
   CaretCircleUp,
   CaretCircleDown,
 } from "@phosphor-icons/react";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { DataTable } from "primereact/datatable";
 // import RadioButtonComponent from "../../components/RadioButtonComponent";
 import FollowUpSchedule from "./followUpSchedule";
 import NotificationInformation from "./notificationInformation";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Toast } from "primereact/toast";
 
 const FollowUp = () => {
   const [searchFollowUp, setSearchFollowUp] = useState<string>();
@@ -20,8 +26,85 @@ const FollowUp = () => {
   const [dateFixDate, setDateFixDate] = useState<Date>();
   const [sendingChannel, setSendingChannel] = useState<string>("");
   const [alertText, setAlertText] = useState<string>("");
+  const [selectedRow, setSelectedRow] = useState({
+    followUpType: "",
+    form: "",
+    duration: "",
+    date: "",
+    sendingChannel: "",
+  });
+  const toast = useRef(null);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (selectedRow) {
+      setIsAccordionOpen(true);
+      if (selectedRow.followUpType == "Dynamic Date") {
+        setIsDynamicDate(true);
+      } else {
+        setIsDynamicDate(false);
+      }
+    }
+  }, [selectedRow]);
+
+  const formOptions = [
+    "CreationDate",
+    "SendDate",
+    "SeenDate",
+    "CompletionDate",
+    "DueDate",
+  ];
+  const sendingChannelOptions = ["SMS", "Email"];
+
+  const followUpData = [
+    {
+      followUpType: "Dynamic Date",
+      duration: "50",
+      form: "SendDate",
+      alertText: "Oops! Something went wrong. Please try again later",
+      sendingChannel: "Email",
+    },
+    {
+      followUpType: "Fix Date",
+      date: "2025-02-11",
+      alertText: "Warning: You have unsaved changes!",
+      sendingChannel: "SMS",
+    },
+    {
+      followUpType: "Fix Date",
+      date: "2025-02-12",
+      alertText: "Caution: This action cannot be undone",
+      sendingChannel: "SMS",
+    },
+    {
+      followUpType: "Dynamic Date",
+      duration: "150",
+      form: "CreationDate",
+      alertText: "Success! Your data has been saved",
+      sendingChannel: "Email",
+    },
+    {
+      followUpType: "Dynamic Date",
+      duration: "8500",
+      form: "DueDate",
+      alertText: "Success! Your profile has been updated",
+      sendingChannel: "Email",
+    },
+    {
+      followUpType: "Fix Date",
+      date: "2025-02-15",
+      alertText: "Oops! We couldn't find what you're looking for",
+      sendingChannel: "SMS",
+    },
+  ];
+
+  const columnDef = [
+    { field: "followUpType", header: "Follow Up Type" },
+    { field: "form", header: "From" },
+    { field: "date", header: "Date" },
+    { field: "duration", header: "Duration (min)" },
+    { field: "sendingChannel", header: "Sending Channel" },
+    { field: "alertText", header: "Alert Text" },
+  ];
 
   const changeSearchBar = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -71,17 +154,8 @@ const FollowUp = () => {
     setIsAccordionOpen(false);
   };
 
-  const formOptions = [
-    "CreationDate",
-    "SendDate",
-    "SeenDate",
-    "CompletionDate",
-    "DueDate",
-  ];
-  const sendingChannelOptions = ["SMS", "Email"];
-
   return (
-    <div className="p-4">
+    <div className="mb-96 p-4">
       <FollowUpSchedule />
       <NotificationInformation />
       <div className="font-bold mb-4">Existing Follow Ups:</div>
@@ -108,7 +182,6 @@ const FollowUp = () => {
               <X color="Black" size={20} onClick={() => setIsSearch(false)} />
             </div>
           )}
-
           <div className="bg-[#2a579a] text-white font-semibold py-2 pl-2 pr-5 flex items-center justify-between">
             Follow Up
             {/* Drag a column header and drop it here to group by that column */}
@@ -119,16 +192,41 @@ const FollowUp = () => {
             />
           </div>
 
-          <ul>
-            <li>DataTable</li>
-            <li>DataTable</li>
-            <li>DataTable</li>
-            <li>DataTable</li>
-            <li>DataTable</li>
-            <li>DataTable</li>
-            <li>DataTable</li>
-            <li>DataTable</li>
-          </ul>
+          <DataTable
+            value={followUpData}
+            showGridlines
+            stripedRows
+            selectionMode={"single"}
+            onSelectionChange={(e) => {
+              console.log("SELECTED ROW", e);
+
+              console.log("start .....");
+              if (e.value == selectedRow) {
+                console.log("if .....");
+
+                alert("Hello");
+                return;
+              } else {
+                console.log("else .....");
+
+                setSelectedRow(e.value);
+              }
+              console.log("Finish .....");
+            }}
+            // onDoubleClick={(e) => {
+            //   e.preventDefault();
+            //   // setForm("");
+            //   // setDuration("");
+            //   // setDateFixDate("");
+            //   // setSendingChannel("");
+            //   // setAlertText("");
+            // }}
+            selection={selectedRow}
+          >
+            {columnDef.map((column) => (
+              <Column field={column.field} header={column.header}></Column>
+            ))}
+          </DataTable>
         </div>
         <div className="flex item-center justify-end m-2 ">
           <button
@@ -178,13 +276,20 @@ const FollowUp = () => {
               </div>
 
               <div
-                id="form"
                 className={`flex items-center ${
                   !isDynamicDate && "pointer-events-none opacity-40"
                 }`}
               >
                 <label className="font-bold text-sm opacity-50">Form: </label>
-                <select name="form" onChange={(e) => changeForm(e)}>
+                <select
+                  name="form"
+                  onChange={(e) => changeForm(e)}
+                  value={
+                    selectedRow.form != undefined || selectedRow.form != null
+                      ? selectedRow.form.toLowerCase()
+                      : form
+                  }
+                >
                   {formOptions.map((option) => (
                     <option value={option.toLowerCase()}>{option}</option>
                   ))}
@@ -201,7 +306,9 @@ const FollowUp = () => {
                 <input
                   type="text"
                   name="duration"
-                  value={duration}
+                  value={selectedRow ? selectedRow.duration : duration}
+                  // value={duration}
+                  defaultValue={selectedRow.duration}
                   onChange={changeDuraiton}
                   className="border-b border-b-black focus:outline-none focus:ring-0"
                 />
@@ -225,12 +332,20 @@ const FollowUp = () => {
                 className={`flex items-center ${
                   isDynamicDate && "pointer-events-none opacity-40"
                 }`}
-                id="form"
               >
                 <label className="font-bold text-sm opacity-50">
                   interDate:
                 </label>
-                <input type="date" name="fixDate" onChange={changeFixDate} />
+                <input
+                  type="date"
+                  name="fixDate"
+                  value={
+                    selectedRow.date
+                      ? selectedRow.date.toString().split("/")[0]
+                      : ""
+                  }
+                  onChange={changeFixDate}
+                />
               </div>
             </div>
           </div>
@@ -240,15 +355,21 @@ const FollowUp = () => {
             Notification Information
             <div className="grid grid-cols-3 gap-2 justify-around items-cener">
               <div className="mb-3">
-                <select
-                  className="w-full"
-                  onChange={(e) => changeSendingChannel(e)}
-                >
-                  {sendingChannelOptions.map((option) => (
-                    <option value={option.toLowerCase()}>{option}</option>
-                  ))}
-                  <option value="">Email</option>
-                </select>
+                <div className="grid grid-cols-3 justify-center items-center">
+                  <label className="col-span-2 text-sm">Sending Channel:</label>
+                  <select
+                    value={
+                      selectedRow.sendingChannel
+                        ? selectedRow.sendingChannel.toLowerCase()
+                        : sendingChannel
+                    }
+                    onChange={(e) => changeSendingChannel(e)}
+                  >
+                    {sendingChannelOptions.map((option) => (
+                      <option value={option.toLowerCase()}>{option}</option>
+                    ))}
+                  </select>
+                </div>
                 <div className="flex h-full gap-2 items-center justify-around">
                   <button
                     className="bg-[#0000cd] text-white font-bold w-full py-1 rounded"
@@ -271,6 +392,7 @@ const FollowUp = () => {
                 <textarea
                   className="w-full focus:outline-0 focus:border-none border-b"
                   maxLength={350}
+                  value={selectedRow ? selectedRow.alertText : alertText}
                   onChange={(e) => changeAlertText(e)}
                 ></textarea>
               </div>
